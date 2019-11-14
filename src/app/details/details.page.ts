e54567cbd956e584389de9a1e8323ab4e063b175
Sergii Rei <srei@corevalue.net>
2019-11-14 12:14:31 +0200

    add to firebase

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -1,4 +1,8 @@
+import { EventsService } from './../events.service';
+import { Acknowledgement } from './../interfaces';
 import { Component, OnInit } from '@angular/core';
+import { EventResponse, EmergencyEvent } from '../interfaces';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.page.scss'],
 })
 export class DetailsPage implements OnInit {
+  eventId: number;
+  eventResponse: EventResponse;
+  event: EmergencyEvent;
+  acknowledgements: Acknowledgement[] = [];
+  newNote = '';
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private eventsService: EventsService) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    // tslint:disable-next-line: no-string-literal
+    this.eventId = +this.route.snapshot.params['eventId'];
+    this.eventResponse = await this.eventsService.getById(this.eventId).toPromise();
+    this.event = this.eventResponse.event;
+    this.acknowledgements = await this.eventsService.getAcknowledgements(this.eventResponse).toPromise();
   }
 
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { Subscription } from 'rxjs';
+
+import { EventsService } from './../events.service';
+import { EventResponse } from './../interfaces';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy {
+  events: EventResponse[] = [];
+  sub: Subscription;
 
-  constructor() {}
+  constructor(private eventsService: EventsService,
+              private nav: NavController) {}
 
+  ngOnInit(): void {
+    this.sub = this.eventsService.getAll().subscribe(e => this.events.push(e));
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  getEvents(): EventResponse[] {
+    return this.events.sort((a, b) => a.event.created > b.event.created ? -1 : 1);
+  }
+
+  details(response: EventResponse) {
+    this.nav.navigateForward(`/details/${response.event.id}`);
+  }
 }
